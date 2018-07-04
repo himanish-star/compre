34ebf82112a4db62e29fa6c2280ae4bf55bc23e4
Soumya Himanish Mohapatra <shmiitian@gmail.com>
2018-07-04 20:21:33 +0530

    more products templates have been scraped off from the amazon.in website

--- scrapeItemPage.js
+++ scrapeItemPage.js
@@ -7,7 +7,7 @@ function isEmpty(obj) {
   return true;
 }
 
-function retrieveURL(doc) {
+function retrieveImageURL(doc) {
   return (doc.getElementById("landingImage") || doc.getElementById("imgBlkFront")).getAttribute('src');
 }
 
@@ -18,18 +18,38 @@ function retrieveTitle(doc) {
 function retrievePrice(doc) {
   return (doc.getElementById("priceblock_saleprice")
     || doc.getElementById("priceblock_ourprice")
+    || doc.getElementById("priceblock_dealprice")
     || doc.getElementsByClassName("offer-price")[0])
     .innerText.trim();
 }
 
 function retrieveDetails(doc) {
-
+  const featureBullets = doc.getElementById("feature-bullets");
+  let morePoints = doc.getElementById('prodDetails');
+  let featureDetails = [];
+  if(featureBullets) {
+    const points = featureBullets.getElementsByTagName('li');
+    for(point of points) {
+      featureDetails.push(point.innerText.trim());
+    }
+  }
+  let moreDetails = [];
+  if(morePoints) {
+    morePoints = morePoints.getElementsByTagName('table')[0]
+    .getElementsByTagName('td');
+    for(let i = 0; i < morePoints.length; i += 2) {
+      let object = {};
+      object[morePoints[i].innerText.trim()] = morePoints[i+1].innerText.trim();
+      moreDetails.push(object);
+    }
+  }
+  return { moreDetails: moreDetails, featureDetails: featureDetails };
 }
 
 function extractWebPage(doc) {
   return new Promise((resolve, reject) => {
     resolve({
-      url: retrieveURL(doc),
+      imageUrl: retrieveImageURL(doc),
       title: retrieveTitle(doc),
       price: retrievePrice(doc),
       details: retrieveDetails(doc)
